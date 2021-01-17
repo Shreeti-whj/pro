451955d10052887c9e5c87d0c68ea1dfc0c9ef42
Shreeti-whj <72453441+Shreeti-whj@users.noreply.github.com>
2021-01-17 15:12:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,24 @@
+//Create variables here
+var dog
+var happydog
+
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(800, 700);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
